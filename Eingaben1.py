2a5425a66c99073776b3ba29f7d944b9763e30a6
FienDDanielFien <128052044+FienDDanielFien@users.noreply.github.com>
2023-03-16 14:45:39 +0100

    Add files via upload

---
+++ Eingaben1.py
@@ -0,0 +1,9 @@
+text    =   input  ("Geben Sie einen text ein:")   #Einlesen eines Textes in die Variable Text
+print(text)  # Ausgabe unseres ausgelesenen Textes
+ganzeZahl = int(input("geben sie die ganzeZahl ein:"))  #einlesen einer ganzen Zahl
+print(ganzeZahl)    #ausgabe der ganzen Zahl
+Kommazahl = float(input("geben sie die Kommazahl ein:"))  #einlesen einer Kommazahlsd
+print(Kommazahl)    #ausgabe der Kommazahl
+
+
+
